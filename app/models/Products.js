c4b90e4ccf7a8c0f600a316cff7de6797e8e1bfd
Michael Smith <msmithboise@gmail.com>
2018-08-08 14:27:51 -0600

    products built

--- app/models/Products.js
+++ app/models/Products.js
@@ -1,34 +1,33 @@
 console.log("Hello from Products.js")
 
+class Products {
+    constructor() {
 
-class Telecaster {
-    constructor(name, price, quantity) {
-        this.name = name || " "
-        this.price = price || 0
-        this.quantity = quantity || 0
+        this.guitars = [{
+            name: "Telecaster",
+            img: "https://www.fmicassets.com/Damroot/ZoomJpg/10002/0110132850_gtr_frt_001_rr.jpg",
+            price: 900,
+            quantity: 10
+        },
+        {
+            name: "Stratocaster",
+            img: "https://stuff.fendergarage.com/images/Q/a/L/fcwd-products-electric-guitars-stratocaster-01-hero-white-strat.jpg",
+            price: 800,
+            quantity: 10
 
-    }
-}
-
-class Stratacaster {
-    constructor(name, price, quantity) {
-        this.name = name || " "
-        this.price = price || 0
-        this.quantity = quantity || 0
-
-    }
-}
-
-class Jazzmaster {
-    constructor(name, price, quantity) {
-        this.name = name || " "
-        this.price = price || 0
-        this.quantity = quantity || 0
-
-    }
-}
+        },
+        {
+            name: "Jazzmaster",
+            img: "https://www.fmicassets.com/Damroot/ZoomJpg/10001/0113092785_gtr_frt_001_rr.jpg",
+            price: 700,
+            quantity: 10
+        }]
+        /// this.guitars array ends
+    } /// constructor ends
+    /// class ends
 
 
-export default Telecaster
-export default Stratacaster
-export default Jazzmaster
+
+
+
+    export default Products
